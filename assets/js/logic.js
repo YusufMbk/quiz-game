131e1c88d144e0b1f81361a438f92475b67b8d95
Yusuf <yusf.mbk@gmail.com>
2023-03-30 17:20:21 +0100

    finished coding the logic and added some questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -24,31 +24,131 @@ let feedBackElement = document.getElementById("feedback");
 
 let sfx = new Audio("assets/sfx/correct.wav")
 
-function questionOnClick(){
 
-}
+function questionOnClick(){
+    console.log("question was clicked");
+    if(this.value !== questions[currentQuestion].answer){
+        time -= 10;
+
+        if(time<0){
+            time = 0;
+        }
+    
+        timerElement.textContent = time;
+
+        feedBackElement.textContent = "Wrong answer";
+    } else {
+        sfxRight.play();
+        feedBackElement.textContent = "You answered correctly!";
+        }
+
+        feedBackElement.setAttribute("class", "feedback");
+
+        setTimeout(function(){
+            feedBackElement.setAttribute("class", "feedback hide")
+        }  , 1500);
+
+        currentQuestion++;
+
+        if(currentQuestion === questions.length) {
+            quizEnd()
+        } else{
+            getQuestion();
+        }
+        }
+
+    
+
 
 function getQuestion(){
 
+    let currentQuestion = questions[currentQuestion];
+
+    let titleElement = document.getElementById("question-title");
+
+    titleElement.textContent = currentQuestion.title;
+
+    choicesElement.innerHTML = "";
+
+    currentQuestion.choices.forEach(function(choice, index){
+
+        var choice = choiceButton = document.createElement("button");
+
+        choiceButton.setAttribute("class", "choice");
+        choiceButton.setAttribute("value", choice);
+
+        choiceButton.textContent = `${index +1}. ${choice}`
+
+        choiceButton.addEventListener("click", questionOnClick);
+
+        choicesElement.append(choiceButton);
+
+    })
+
 }
 
-function clockTick(){
 
+function clockTick(){
+    time--;
+    timerElement.textContent = time;
+
+    if(time <= 0){
+        quizEnd();
+    }
 }
 
 function startQuiz(){
+    let startScreenElement = document.getElementById("start-screen");
+    startScreenElement.setAttribute("class", "hide");
 
+    questionsElement.removeAttribute("class");
+
+    timerID = setInterval(clockTick, 1500);
+
+    timerElement.textContent = time;
+
+    getQuestion();
 }
 
 function quizEnd(){
+    clearInterval(timerID);
+
+    let endScreenElement = document.getElementById("end-screen");
+    endScreenElement.removeAttribute("class");
+
+    let finalScoreElement = document.getElementById("final-score");
+    finalScoreElement.textContent = time;
+
+    questionsElement.setAttribute("class" , "hide");
 
 }
 
-function saveHs(){
+
+function saveHighScore(){
+    let initals = initalElement.value.trim();
+
+    if(initals !== ""){
+        let highScores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+        let newScore = {
+            score: time,
+            initals: initals
+        }
+
+        highScores.push(newScore);
+        localStorage.setItem("highscores", JSON.stringify(highScores));
+
+        window.location.href = "highscores.html";
+
+    }
 
 }
 
 function checkForEnter(event){
+
+    if (event.key === "Enter"){
+        saveHighScore();
+    }
 
 }
 
@@ -57,4 +157,5 @@ startButton.addEventListener("click", startQuiz);
 submitButton.addEventListener("click", saveHighScore);
 
 initialElement.addEventListener("keyup", checkForEnter);
+
 
---
+++ assets/js/questions.js
@@ -0,0 +1,18 @@
+let questions = [
+    {
+        "title": "Which of the following is NOT a common Data Type:",
+        "choices": ["String", "Integers", "Decimal", "Alerts"],
+        "answer" : "alerts"
+    },
+
+    {
+        "title": "Which of the following languages is used to style web pages?",
+        "choices": ["HTML", "CSS", "JavaScript", "Java"],
+        "answer" : "CSS"
+    },
+    {
+        "title": "Which of the following languages is not an actual language",
+        "choices": ["Java", "C++", "C", "Boolean"],
+        "answer": "Boolean"
+    }
+]
